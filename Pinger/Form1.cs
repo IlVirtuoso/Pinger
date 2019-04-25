b80ed27bf7c38d24d4e9f9bf3d5d09456eec73da
Virtuoso <Virtuoso@DESKTOP-K7318QU>
2019-04-25 10:32:14 +0200

    update

--- Pinger/Form1.cs
+++ Pinger/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net;
 
 namespace Pinger
 {
@@ -15,6 +17,11 @@ namespace Pinger
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
